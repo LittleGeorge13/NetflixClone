f9af0a52973abd3e714bd2fb69fa564c733c4e1c
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-22 12:53:54 -0600

    Netflix featured movie title

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,63 +1,13 @@
 import { AcUnit } from "@material-ui/icons"
 import "./home.scss"
 import Navbar from "../components/navbar/Navbar"
+import Featured from "../components/featured/Featured"
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-        <img width="100%" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur deleniti quasi deserunt saepe at a. Nam, ullam! Voluptates sunt doloribus eligendi exercitationem? Possimus placeat autem, vel soluta omnis suscipit.
+        <Featured type='movie' />
     </div>
   )
 }
