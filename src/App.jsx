697d0949691bfc2cf4993061fcc75b2f555931f3
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-08-06 23:13:17 -0600

    Fixed styling file import and updated git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
-import "./app"
+import "./app.scss"
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
